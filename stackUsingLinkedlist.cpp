337f2e4df99bf0de0c6a5fd32c03caffab319249
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-05-12 10:57:39 +0700

    membuat variabel

--- stackUsingLinkedlist.cpp
+++ stackUsingLinkedlist.cpp
@@ -4,4 +4,12 @@ using namespace std;
 //node class respresentating a single node in the linked list
 class node
  {
+public:
+    int data;
+    node* next;
+
+    node()
+    {
+        next = NULL;
+    }
 };
