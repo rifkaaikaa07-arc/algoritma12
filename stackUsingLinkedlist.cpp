2581383569a76167019267ee1d79949f57bebb66
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-05-12 11:00:15 +0700

    menambahkana public

--- stackUsingLinkedlist.cpp
+++ stackUsingLinkedlist.cpp
@@ -19,4 +19,19 @@ class stack
 {
     private:
     node* top; // pointer to the top node of the stack
+
+    public:
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        node*newnode = new node();
+        newnode->data = value;
+        newnode ->next = top;
+        top = newnode;
+        cout << "push value: " << value << endl;
+        return value;
+    }
 };
